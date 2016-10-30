eb3d05034b9fcde53c883b1d35e867ca18cf5ca1
Ben Reynoso <breynoso@ucsc.edu>
2016-10-30 16:38:29 -0700

    Added CheckingAccount and SavingsAccount super classes to ps02

---
+++ workspace0/ps02/src/SavingsAccount.java
@@ -0,0 +1,10 @@
+
+public class SavingsAccount extends BankAccount {
+	
+	private int interestRate;
+	private int maxNumWithdrawals;
+	
+	public SavingsAccount() {
+		setAccountType("savings");
+	}
+}
