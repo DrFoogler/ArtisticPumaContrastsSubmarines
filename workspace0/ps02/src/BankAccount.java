1dfc36427ca4fa4420568a06e037c68e8c045432
Benjamin Reynoso <bereynos@ucsc.edu>
2016-10-29 20:39:15 -0700

    Created BankAccount class

--- workspace0/ps02/src/BankAccount.java
+++ workspace0/ps02/src/BankAccount.java
@@ -1,3 +1,20 @@
 public class BankAccount {
+	
+	private int balance;
+	private String ownerSSN;
+	private String accountType;
+	
+	
+	public BankAccount() {
+		
+	}
+		
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(int newBalance) {
+		balance = newBalance;	
+	}
 
 }
---
+++ workspace0/ps02/src/BankAccountDriver.java
@@ -0,0 +1,15 @@
+
+public class BankAccountDriver {
+
+	public static void main(String[] args) {
+		
+		
+		BankAccount acct1 = new BankAccount();
+		
+		acct1.setBalance(2305);
+		acct1.ownerSSN = "228-82-1105";
+		acct1.accountType = "checking";
+		
+	}
+
+}
