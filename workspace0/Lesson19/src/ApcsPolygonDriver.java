558492313d377c602a994887aa8553842eb6e04f
Benjamin Reynoso <bereynos@ucsc.edu>
2016-11-09 17:32:03 -0800

    Created instance of  ApcsPolygon and ApcsPolygonDriver for Lesson19

---
+++ workspace0/Lesson19/src/ApcsPolygonDriver.java
@@ -0,0 +1,13 @@
+
+public class ApcsPolygonDriver {
+
+	public static void main(String[] args) {
+		ApcsPolygon poly1 = new ApcsPolygon();
+		
+		poly1.setNumSides(8);
+		
+		System.out.println(poly1);
+		
+	}
+
+}
