ab77ab822cd3f5e6667bf880d160b381a099e173
Benjamin Reynoso <bereynos@ucsc.edu>
2016-11-21 22:40:53 -0800

    Lesson 21 Do Now Slide

---
+++ workspace0/Lesson21/src/DoNow.java
@@ -0,0 +1,14 @@
+public class DoNow {
+	public static void main(String[] args) {
+		int i = 0;
+		int j = 1;
+		int a = 10;
+		while ( i < j ) {
+		System.out.println(a);
+		i++;
+		j++;
+		a--;
+		}
+	}
+
+}
